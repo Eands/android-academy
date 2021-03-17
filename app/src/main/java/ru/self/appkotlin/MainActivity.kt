7803726f50fd39d6c8f2cf3a51953a4af861ed35
Andrey Evseev <an.evseev94@gmail.com>
2021-03-17 18:15:10 +0600

    homework2

--- app/src/main/java/ru/self/appkotlin/MainActivity.kt
+++ app/src/main/java/ru/self/appkotlin/MainActivity.kt
@@ -9,27 +9,6 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.title)
-
-        textView.setOnClickListener {
-            openSecondActivity()
-        }
-    }
-
-    private fun openSecondActivity() {
-        val intent = Intent(this, SecondActivity::class.java)
-        val s = "transmittedString"
-        val s1 = "transmittedInt"
-        val s2 = "transmittedBool"
-        intent.putExtra(s, "string to transmit")
-        intent.putExtra(s1, 12)
-        intent.putExtra(s2, false)
-        startActivity(intent)
-//        intent.putExtra("KEY", 1)
-//        Log.i("info", "Info")
-//        startActivity(intent)
-//        startActivityForResult()
+        setContentView(R.layout.activity_movie_details)
     }
 }
