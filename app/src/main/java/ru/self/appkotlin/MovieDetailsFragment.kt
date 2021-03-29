ec0dbe8ce0b62f5bff0ca9f3f1b61904fd360a44
Andrey Evseev <an.evseev94@gmail.com>
2021-03-29 22:05:16 +0600

    works

--- app/src/main/java/ru/self/appkotlin/MovieDetailsFragment.kt
+++ app/src/main/java/ru/self/appkotlin/MovieDetailsFragment.kt
@@ -1,15 +1,23 @@
 package ru.self.appkotlin
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class MovieDetailsFragment : Fragment() {
 
     private var backListener: MovieDetailsBackClickListener ?= null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is MovieDetailsBackClickListener) {
+            backListener = context
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_movie_details, container, false)
@@ -18,13 +26,12 @@ class MovieDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<TextView>(R.id.back_button_text).apply {
+        view.findViewById<View>(R.id.back_button_text).apply {
             setOnClickListener { backListener?.onMovieDeselected() }
         }
-//        view.findViewById<TextView>(R.id.back_button_text)?.setOnClickListener {
-//            backListener?.onMovieDeselected()
-//        }
     }
+
+
 
     interface MovieDetailsBackClickListener {
         fun onMovieDeselected()
--- app/src/main/java/ru/self/appkotlin/MoviesListFragment.kt
+++ app/src/main/java/ru/self/appkotlin/MoviesListFragment.kt
@@ -1,15 +1,23 @@
 package ru.self.appkotlin
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 
 class MoviesListFragment: Fragment() {
 
     private var moviesClickListener: MoviesListItemClickListener ?= null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is MoviesListItemClickListener) {
+            moviesClickListener = context
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_movies_list, container, false)
@@ -18,12 +26,9 @@ class MoviesListFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<ConstraintLayout>(R.id.movies_list_item_layout).apply {
+        view.findViewById<View>(R.id.movies_list_item_layout).apply {
             setOnClickListener {moviesClickListener?.onMovieSelected()}
         }
-//        view.findViewById<ConstraintLayout>(R.id.movies_list_item_layout)?.setOnClickListener {
-//            moviesClickListener?.onMovieSelected()
-//        }
     }
 
     interface MoviesListItemClickListener {
